c8089f54296dde462e0eeeea2c7de07aac8e8168
HentaiVN <115784927+HentaiVN210@users.noreply.github.com>
2022-10-14 16:39:39 +0700

    Add files via upload

---
+++ PC_buff.user.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name         Bú bẩn thịnh hành
+// @namespace    https://hentaivn.la/
+// @updateURL    
+// @version      2.0.0
+// @description  Bú bú bú
+// @author       Ừ em
+// @include      *
+// @require      http://code.jquery.com/jquery-2.1.0.min.js
+// @match        https://hentaivn.la/
+// @run-at       document-start
+// @icon         https://cdn.discordapp.com/attachments/994833515554287666/1027189547114700830/guralacdit.gif
+// @grant        none
+// ==/UserScript==
+
+function buff(n) {
+    const id = document.querySelector("#myInputxx").value;
+    for (let i = 1; i<=n; i++){
+        let dom = document.querySelector(".bookmark")
+        dom.innerHTML = `<span class="bookmark"><a id="click" href="javascript:;" onclick="doAction('${id}','bookmark');">Theo dõi ngay</a></span>`
+        document.querySelector("#click").click();
+        console.log("bú");
+    };
+};
+
+document.addEventListener ("DOMContentLoaded", function(){
+    if(!document.URL.includes("doc-truyen")){
+        return console.log("idle...");
+    };
+    console.log("sẵn sàng bú bẩn")
+    const page = document.querySelector('.page-info');
+    const id = document.querySelector("#myInputxx").value;
+    var newScript = document.createElement("script");
+    newScript.innerHTML = `function doRead(o,t){$("#imgLoader_"+o).css("width","92px"),$.post("ajax_bookmark_topic.php",{book_id:o,book_type:t},function(t){$(".read_"+o).html(t)})}function doRemove(o,t){$.post("ajax_bookmark_topic.php",{book_id:o,book_type:t},function(t){$(".remove_"+o).html(t)})}`;
+    page.prepend(newScript);
+
+    var newp = document.createElement('p');
+    newp.innerHTML = `<input type="number" placeholder="Số lượt bú" name="quantity" min="1" max="200" style="width: 7em"> <input class="bb" type="submit" value="Bú bẩn" style="text-align: center; width: 5em"> <input class="rm" type="submit" value="Xoá" style="text-align: center; width: 3em" onclick="doRemove('${id}','remove');" href="javascript:;">`;
+    page.appendChild(newp);
+
+    // var newa = document.createElement('p');
+    // newa.innerHTML = `<input class="rm" type="submit" value="Xoá" style="text-align: center" onclick="doRemove('${id}','remove');" href="javascript:;">`;
+    // page.appendChild(newa);
+
+    $(document).delegate( '.bb', "click", function(){
+        //đoạn này để check truyện của phải của sp không, ai xoá làm chó kkk
+        const sp = page.textContent;
+        if(sp.includes('Sexual Paradise')){
+            return alert("Có cặc tao búp cho sp!");
+        };
+        try{
+            var n = document.querySelector("input[type='number']").value;
+            buff(n);
+            //alert("Xong!");
+            if(confirm("Xong! Bạn có muốn xoá truyện này khỏi danh sách theo dõi ngay không?")){
+                doRemove(id,'remove');
+                alert("Đã xoá!");
+            };
+        }
+        catch(e){
+            console.log(e);
+        }
+    });
+});
---
+++ README.md
@@ -0,0 +1,23 @@
+# Script búp bẩn tab thịnh hành tại [HentaiVN.la](https://hentaivn.la/)
+Bấm F5 mỏi tay quá nên tôi viết cái script để đỡ phải bấm nhiều.
+
+# UPDATE & NEWS
+- Vì một lý do củ c*c nào đó mà tôi vẫn chưa rõ, tạm thời phiên bản mobile không hoạt động (búp chán chê không lên top), còn PC vẫn bình thường nhé.
+- Tách riêng PC và mobile cho nó nhiều.
+- Thêm nút `Xoá` (bỏ theo dõi) ngay tại trang truyện.
+- Thêm tính năng tự động bỏ theo dõi sau khi buff (chưa tối ưu). 
+
+# Install
+1. Cài đặt [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo).
+2. Nhấp [vào đây nếu dùng PC]() hoặc [vào đây nếu dùng điện thoại]() và bấm `Install` để cài đặt script.
+
+# Usage
+Vào truyện nào đó mà dùng thử → Tự hiểu cách dùng (và nhớ phải đăng nhập acc vào mới dùng được)!
+
+# Note
+1. Nếu muốn sử dụng trên điện thoại, cần cài đặt [Kiwi Browser](https://play.google.com/store/apps/details?id=com.kiwibrowser.browser) và dùng Kiwi để thực hiện các bước kể trên.
+2. ***KHÔNG*** thể buff truyện có chủ thớt là Uploader hoặc chính acc đang đăng nhập.
+3. Sau khi buff, [danh sách truyện theo dõi](https://hentaivn.la/bookmark-list.php) sẽ bị lỗi lặp truyện, nếu muốn fix thì chỉ cần xoá truyện bị lặp 1 lần là được.
+
+# How it works?
+Tự vào source code mà xem, tôi biết là tôi code ngu rồi nên cứ chê thoải mái.
